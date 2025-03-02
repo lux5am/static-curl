71b9912e938b5664a9783cfa34c22f69f4470b23
lux5am <lux.5am@yandex.com>
2025-03-03 02:36:40 +0800

    update shared

--- curl-shared-cross.sh
+++ curl-shared-cross.sh
@@ -157,6 +157,8 @@ install_cross_compile() {
            STRIP="${DIR}/${SOURCE_DIR}/bin/${SOURCE_DIR}-strip" \
            AS="${DIR}/${SOURCE_DIR}/bin/${SOURCE_DIR}-as" \
            PATH="${DIR}/${SOURCE_DIR}/bin":"${DIR}/${SOURCE_DIR}/${SOURCE_DIR}/bin":"$PATH"
+
+    apt-get install -y "${LIBC}"
     ls -l "${DIR}/${SOURCE_DIR}/bin/${SOURCE_DIR}-as"
     file "${DIR}/${SOURCE_DIR}/bin/${SOURCE_DIR}-as"
     ldd "${DIR}/${SOURCE_DIR}/bin/${SOURCE_DIR}-as"
